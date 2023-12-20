9e5217a06ac89037964c63cf0bc1c4290f23df03
Matus-SK <Matus_SK@users.noreply.github.com>
2023-12-20 18:53:38 +0100

    Change structure from OOP bc obviosly I am dumb

--- French_Bank.py
+++ French_Bank.py
@@ -1,26 +1,29 @@
 from random import randint
 
-#class for dice
-class Dice:
-    def __init__(self, name):
-        self.name = name
-    #random number generator for our dice
-    def dice_toss(self):
-        return randint(1,6)
-      
 
 
-dice_1 = Dice("1")
-dice_2 = Dice("2")
-dice_3 = Dice("3")
+def round_sumary(round_sum):
+        if round_sum >= 5 and round_sum <= 7:
+            print("Low Bet Win!")
+        elif round_sum >= 14 and round_sum <= 16:
+            print("High Bet Win!")
+        elif dice_1.dice_toss() == dice_2.dice_toss() == dice_3.dice_toss():
+            print("Ace Bet Win!")
+        else:
+            print("No one wins, new toss.")
 
-round_sum = dice_1.dice_toss() + dice_2.dice_toss() + dice_3.dice_toss()
+dice_1 = randint(1,6)
+dice_2 = randint(1,6)
+dice_3 = randint(1,6)
+player_name = ""
+player_bank = ""
+casino_bank = 10000
+game_on = True
 
-if round_sum >= 5 and round_sum <= 7:
-    print("Low Bet Win!")
-elif round_sum >= 14 and round_sum <= 16:
-    print("High Bet Win!")
-elif dice_1.dice_toss() == dice_2.dice_toss() == dice_3.dice_toss():
-    print("Ace Bet Win!")
-else:
-    print("No one wins, new toss.")
+while game_on:
+     break
+     
+round_sum = dice_1 + dice_2 + dice_3
+
+round_sumary(round_sum)
+
