46cddccd35dd29b130d4061f394f421190256a2b
Matus-SK <Matus_SK@users.noreply.github.com>
2023-12-20 20:00:51 +0100

    add function for player bet input

--- French_Bank.py
+++ French_Bank.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-
+#function to decide witch bet wins
 def round_sumary(round_sum):
         if round_sum >= 5 and round_sum <= 7:
             print("Low Bet Win!")
@@ -11,7 +11,7 @@ def round_sumary(round_sum):
             print("Ace Bet Win!")
         else:
             print("No one wins, new toss.")
-
+#function that veritify bank balance
 def bet_verit(bet, bank):
     while True:
         try:
@@ -24,11 +24,25 @@ def bet_verit(bet, bank):
         except:
             print("Sorry, only numeric value.")
 
+#function that handle player choice:
+def player_choice_bet():
+    print("For low bet type A\nFor high bet type B\nFor Ace type C")
+    while True:
+        choice_input = input("What is your choice? ").lower()
+        try:
+            if choice_input == "a" or choice_input == "b" or choice_input == "c":
+                return choice_input
+            else:
+                print("Sorry, wrong choice, choose only 'A', 'B', 'C'!")
+        except:
+            print("Sorry, wrong choice, choose only 'A', 'B', 'C'!")
+
 
 
 player_name = ""
 player_bank = 0
 player_bet = 0
+player_choice = ""
 game_on = True
 
 print("""Hello! Welcome to our game of French Bank.
@@ -53,11 +67,12 @@ while True:
 
 
 while player_bank > 0:
+    print(f"Welcome to new round! Your current bank balance is {player_bank}")
     dice_1 = randint(1,6)
     dice_2 = randint(1,6)
     dice_3 = randint(1,6)
     
-
+    player_choice = player_choice_bet()
     player_bet = bet_verit(player_bet, player_bank)
     
 
